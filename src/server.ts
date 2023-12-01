bc0defba6e994e9c46e6daacb85094b863ebabfc
mostafizur-raahman <mostafizurrahman0202@gmail.com>
2023-12-02 05:50:54 +0600

    eslint and some config change

--- src/server.ts
+++ src/server.ts
@@ -5,11 +5,14 @@ import app from "./app";
 // admin-um
 // adminMOSTAFIZ-um
 async function server() {
-    await mongoose.connect(config.database_url as string);
-    app.listen(config.port, () => {
-        console.log(`Example app listening on port ${config.port}`);
-    });
+    try {
+        await mongoose.connect(config.database_url as string);
+        app.listen(config.port, () => {
+            console.log(`app listening on port ${config.port}`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
 }
 
-server().catch((err) => console.log(err));
 server();
